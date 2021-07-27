0b002ef5cfde1f8e3e1d4301792d06982ab1a13d
LuisSigala <a6519150031@utch.edu.mx>
2021-07-27 13:51:58 -0600

    Landing design and css desing

---
+++ src/Components/Landing/Landing.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+} from 'react-native'
+import Colors from '../../res/Colors'
+
+class Landing extends React.Component {
+    handlePress = () => {
+        this.props.navigation.navigate('Profile');
+    };
+
+    render() {
+        return (
+            <View style={styles.landContainer}>
+                <View style={styles.landLayerColor}>
+                    <Text style={styles.landInputText}>Feeling Like Recycling Today?</Text>
+                    <Text style={styles.landInputText1}>
+                        Find your nearest recycling station now
+                        start recycling and earn wonderful rewards
+                    </Text>
+                    <TouchableOpacity style={styles.landButton} onPress={this.handlePress}>
+                        <Text style={styles.landButtonText}>ScoreBoards</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.landButton} onPress={this.handlePress}>
+                        <Text style={styles.landButtonText}>Record</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.landButton} onPress={this.handlePress}>
+                        <Text style={styles.landButtonText}>Map</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.landButton} onPress={this.handlePress}>
+                        <Text style={styles.landButtonText}>Goals</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    landContainer: {
+        flex: 1,
+        backgroundColor: Colors.white,
+    },
+    landInputText: {
+        width: '90%',
+        fontSize: 25,
+        fontWeight: 'bold',
+        marginTop: '-1%',
+        textAlign: 'center',
+        color: Colors.white,
+    },
+    landInputText1: {
+        width: '80%',
+        fontSize: 20,
+        marginTop: '1%',
+        marginLeft: '-5%',
+        textAlign: 'left',
+        color: Colors.white,
+    },
+    landLayerColor: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.greenLight,
+    },
+    landButton: {
+        backgroundColor: Colors.blueDark,
+        color: Colors.white,
+        width: '80%',
+        height: '13%',
+        padding: 15,
+        marginTop: '8%',
+        borderRadius: 50,
+        borderWidth: 1,
+    },
+    landButtonText: {
+        marginTop: '2%',
+        textAlign: 'center',
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: Colors.white,
+    },
+});
+
+export default Landing;
