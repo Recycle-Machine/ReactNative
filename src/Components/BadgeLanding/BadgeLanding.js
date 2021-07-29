b7b3281fac168cdd0be1592175dd6888a9090136
LuisSigala <a6519150031@utch.edu.mx>
2021-07-28 22:14:30 -0600

    Links changes for the files move to other folders

--- src/Components/BadgeLanding/BadgeLanding.js
+++ src/Components/BadgeLanding/BadgeLanding.js
@@ -14,7 +14,7 @@ import Colors from '../../res/Colors'
 
     class BadgeLanding extends React.Component{
         handlePress = () => {
-            this.props.navigation.replace('Machine');
+            this.props.navigation.replace('Yearly Goals');
         };
 
         render(){
