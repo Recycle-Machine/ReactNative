a646ea8a118d7affa12efbb3e4396656739960eb
LuisSigala <a6519150031@utch.edu.mx>
2021-07-29 23:32:05 -0600

    ScoreSchool design, css desing and ScoreDesing added

---
+++ src/Components/ScoreBoards/ScoreSchool.js
@@ -0,0 +1,75 @@
+import React from 'react'
+import { 
+    View, 
+    ActivityIndicator, 
+    StyleSheet, 
+    FlatList,
+    StatusBar,
+} from 'react-native'
+import ScoreDesign from './ScoreDesign'
+import Colors from '../../res/Colors'
+import Http from '../../libs/http'
+
+class ScoreSchool extends React.Component{
+    state = {
+        loading: false,
+        badges: undefined,
+    }
+    
+    componentDidMount(){
+        this.fetchdata();
+    }
+    fetchdata = async() => {
+        console.log('Fetching data');
+        this.setState({loading: true});
+        let response = await Http.instance.get_all();
+        this.setState({loading: false, badges: response});
+    };
+
+
+
+    render(){
+        const {badges, loading} = this.state;
+        
+        return(
+            <View style={[styles.container, styles.horizontal]}>
+                {loading ? (
+                    <ActivityIndicator
+                        style={styles.loader}
+                        color={Colors.blueDark}
+                        size="large"
+                    />
+                ) : null}
+                <StatusBar backgroundColor="transparent" translucent={true} />
+                <FlatList 
+                style={styles.list} 
+                data={badges} 
+                renderItem={({item}) => <ScoreDesign item={item} />}
+                keyExtractor = {(item, index) => index.toString()}
+                />  
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: Colors.black,
+        paddingTop: 30,
+    },
+    horizontal:{
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    loader:{
+        height: '100%',
+        paddingHorizontal: 10,
+    },
+    list:{
+        width: '100%',
+        paddingHorizontal: 10,
+    },
+});
+
+export default ScoreSchool;
